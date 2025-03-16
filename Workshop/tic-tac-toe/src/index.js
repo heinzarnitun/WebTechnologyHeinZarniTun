e34549b417ef46994c612d433e89e3420bcfe90c
heinzarnitun <heinzarnitun1@gmail.com>
2025-03-16 14:43:10 +0630

    tic tac toe

---
+++ Workshop/tic-tac-toe/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Capitalize 'ReactDOM'
+import './styles.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Fix the typo here
+root.render(<App />);
